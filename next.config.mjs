824713e4352c8973b192bad41b78b8cd40fa45f5
Federico Molas <flmolas@tu.com.py>
2024-06-17 11:13:06 -0400

    nexyt

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
     images: {
       domains: ['cdn-icons-png.flaticon.com'],
     },
+    output: "export",  // <=== enables static exports
+  reactStrictMode: true,
   };
   
   export default nextConfig;
